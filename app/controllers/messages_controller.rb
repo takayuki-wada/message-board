f62afe151f4a748bea2b85391e39468e98c70348
takayuki wada <annapurna.k2.d@gmail.com>
2017-02-26 05:15:26 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,27 @@
 class MessagesController < ApplicationController
+  # フォームを表示するアクションの定義
   def index
+  	@message = Message.new
+  	# Messageをすべて取得
+		@messages = Message.all
+  end
+
+  # パラメータにmessageキーがあれば成功
+  # messageキーがなければ、ApplicationController:Baseにキャッチされ
+  # 400 Bad Requestを返す
+  def create
+		@message = Message.new(message_params)
+		@message.save
+		redirect_to root_path, notice: 'メッセージを保存しました'
+
+  end
+
+	private
+  # 許可するパラメータをカプセル化する
+  # 許可する属性をメッセージ毎にちぇっくするようこのメソッドを特殊化
+  def message_params
+  	# paramsがmessageというKeyが存在するか検証し、params[:message]のうち、
+  	# :nameおよび:bodyの値のみ受取るようにフィルタリング
+  	params.require(:message).permit(:name, :baody)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'messages/index'
-  # root 'messages#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -21,6 +20,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+	resources :messages, only: [:create]
 
   # Example resource route with options:
   #   resources :products do
