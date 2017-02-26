23e6e524b49b883b409d280e9413d43215ad9497
takayuki wada <annapurna.k2.d@gmail.com>
2017-02-26 06:54:30 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,9 @@
 class MessagesController < ApplicationController
   # フォームを表示するアクションの定義
   def index
+  	# Viewのform_forに渡すことでフォームが動作する
   	@message = Message.new
-  	# Messageをすべて取得
+  	# Messageをすべて取得する
 		@messages = Message.all
   end
 
@@ -13,15 +14,14 @@ class MessagesController < ApplicationController
 		@message = Message.new(message_params)
 		@message.save
 		redirect_to root_path, notice: 'メッセージを保存しました'
-
   end
 
 	private
   # 許可するパラメータをカプセル化する
-  # 許可する属性をメッセージ毎にちぇっくするようこのメソッドを特殊化
+  # 許可する属性をメッセージ毎にチェックするようこのメソッドを特殊化
   def message_params
   	# paramsがmessageというKeyが存在するか検証し、params[:message]のうち、
   	# :nameおよび:bodyの値のみ受取るようにフィルタリング
-  	params.require(:message).permit(:name, :baody)
+  	params.require(:message).permit(:name, :body)
   end
 end
