53caef5c6c25fda88bcb778baf067315d13cd641
takayuki wada <annapurna.k2.d@gmail.com>
2017-02-28 13:25:33 +0000

    implementation of message board

--- Gemfile
+++ Gemfile
@@ -45,3 +45,5 @@ group :development do
   gem 'spring'
 end
 
+# pry-byebug
+gem 'pry-byebug', group: :development
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,7 @@
 class MessagesController < ApplicationController
+	# set_messageメソッドは、editおよびupdate、destroyアクションの前に実行する(フィルタ機能)
+	before_action :set_message, only: [:edit, :update, :destroy]
+
   # フォームを表示するアクションの定義
   def index
   	# Viewのform_forに渡すことでフォームが動作する
@@ -32,6 +35,29 @@ class MessagesController < ApplicationController
 		end
   end
 
+	def edit
+		# 編集画面に来た時に呼ばれるメソッド(アクション)
+		# 但し、before_actionで定義した通り、最初にset_messageメソッドで処理が開始する
+	end
+
+	def update
+		# set_messageメソッドでセットされたインスタンス変数を用いて更新処理を行う
+		if @message.update(message_params)
+			# 保存に成功した場合は、トップページへリダイレクト
+			redirect_to root_path, notice: 'メッセージを編集しました'
+		else
+			# 保存に失敗した場合は、編集画面へ戻す(/views/messages/edit.html.erbを呼出す)
+			render 'edit'
+		end
+	end
+
+	def destroy
+		# インスタンス変数に格納されたデータを削除
+		@message.destroy
+		# トップ画面への遷移とデータを削除したメッセージを出力
+		redirect_to root_path, notice: 'メッセージを削除しました'
+	end
+
 	private
   # 許可するパラメータをカプセル化する
   # 許可する属性をメッセージ毎にチェックするようこのメソッドを特殊化
@@ -39,5 +65,10 @@ class MessagesController < ApplicationController
   	# paramsがmessageというKeyが存在するか検証し、params[:message]のうち、
   	# :nameおよび:bodyの値のみ受取るようにフィルタリング
   	params.require(:message).permit(:name, :body)
+  end
+
+  def set_message
+  	# 編集しようとする投稿の内容を@messageにセットし、edit.html.erbをレンダリング
+  	@message = Message.find(params[:id])
   end
 end
